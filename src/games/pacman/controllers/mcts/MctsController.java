42e727c0d610e1a7c10a228d71503360e1c30564
andre <andremlsantos@gmail.com>
2018-01-02 22:04:29 +0000

    MCTS
    test commit for the new MCTS AI

---
+++ src/games/pacman/controllers/mcts/MctsController.java
@@ -0,0 +1,14 @@
+package games.pacman.controllers.mcts;
+
+import screenpac.controllers.AgentInterface;
+import screenpac.model.GameStateInterface;
+
+public class MctsController implements AgentInterface{
+
+	@Override
+	public int action(GameStateInterface gs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
